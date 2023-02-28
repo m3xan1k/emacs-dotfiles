4aa59c8da9b6a51628a471e560aa4666dd0c3207
m3xan1k <m3xan1k@duck.com>
2023-02-28 18:27:12 +0300

    multiple cursors

--- init.el
+++ init.el
@@ -16,7 +16,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
   '(package-selected-packages
-     '(move-text expand-region helm-ag helm fzf ripgrep ag projectile telega ace-window counsel tabbar which-key try company-jedi restclient-jq restclient lsp-treemacs cider clojure-mode yasnippet company lsp-ui go-mode lsp-mode racket-mode true use-package almost-mono-themes lsp-pyright treemacs git-gutter)))
+     '(multiple-cursors move-text expand-region helm-ag helm fzf ripgrep ag projectile telega ace-window counsel tabbar which-key try company-jedi restclient-jq restclient lsp-treemacs cider clojure-mode yasnippet company lsp-ui go-mode lsp-mode racket-mode true use-package almost-mono-themes lsp-pyright treemacs git-gutter)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -234,6 +234,11 @@
   :ensure t
   :config
   (move-text-default-bindings))
+
+(use-package multiple-cursors
+  :ensure t
+  :config
+  (global-set-key (kbd "C-d") 'mc/mark-next-like-this-word))
 
 ;; VARS
 (setq indo-enable-flex-matching t)
